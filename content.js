9446e89562bcaf6cb855b13dba8b3f46426659a8
sidtiwari98 <sidtiwari98@gmail.com>
2020-05-22 21:37:51 +0530

    chrome extension starting guide setup for learning

---
+++ content.js
@@ -0,0 +1,10 @@
+// alert("Hello from your Chrome extension!")
+//in this file add logic of spotting the highlighted text
+// var text = "";
+// if (window.getSelection) {
+//   text = window.getSelection().toString();
+//   alert(text)
+// }
+// else{
+//   alert("No text")
+// }
