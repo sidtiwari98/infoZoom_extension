d35d259c9200b8da869c229e0a8d47f4eb0bec8a
sidtiwari98 <sidtiwari98@gmail.com>
2020-05-29 23:55:22 +0530

    Styling and word from page stored

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+  chrome.storage.sync.set({selectedText: ''}, function() {
+    console.log("selected text is set")
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
@@ -12,3 +12,4 @@ chrome.runtime.onInstalled.addListener(function() {
     }]);
   });
 });
+
--- popup.js
+++ popup.js
@@ -1,15 +1,31 @@
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
+//
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
+//
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.executeScript(
+//       tabs[0].id,
+//       {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//   });
+// };
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+document.getElementById("search_button").addEventListener("click", getSelectedText);
+
+function getSelectedText() {
+  // var selectedText = '';
+  chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+  }, function(selection) {
+    chrome.storage.sync.get('selectedText', function(data) {
+      data.selectedText = selection[0];
+      document.testform.selectedtext.value =  data.selectedText;
 });
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    // selectedText = selection[0];
+    // document.testform.selectedtext.value = selectedText;
   });
-};
+}
