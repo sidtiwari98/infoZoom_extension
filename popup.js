ce458f2314a9be7a84a81ae44f81ad38c4e1c7e0
sidtiwari98 <sidtiwari98@gmail.com>
2020-06-04 17:06:03 +0530

    better structure

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ function getSelectedText() {
   chrome.storage.sync.get('selectedText', function(data) {
     //for testing if the highlighted word is stored
     // document.testform.selectedtext.value = data.selectedText;
+    console.log(data.selectedText);
     textToSearch = data.selectedText;
     if(textToSearch.length) {
       SearchImage();
@@ -14,7 +15,7 @@ function getSelectedText() {
 async function SearchImage(){
   let response = await fetch(`https://www.googleapis.com/customsearch/v1?q=${textToSearch}&key=AIzaSyAkCdD1ro_UiO05w8iqAdFjNCCCbgqnSlY&num=7&start=1&searchType=image&imgSize=medium&cx=014315630586445032606:me0hgspmvrs`);
   let data = await response.json();
-  console.log(data.items[0].image.thumbnailLink,data.items[1].image.thumbnailLink, data.items[2].image.thumbnailLink);
+  // console.log(data.items[0].image.thumbnailLink,data.items[1].image.thumbnailLink, data.items[2].image.thumbnailLink);
   setImage(data.items[0].image.thumbnailLink,data.items[1].image.thumbnailLink, data.items[2].image.thumbnailLink);
   // console.log(data);
 }
