da76131b5ba32aa525987ee2161af9d96c6d8d08
sidtiwari98 <sidtiwari98@gmail.com>
2020-06-04 16:42:38 +0530

    image api and display now works

--- popup.js
+++ popup.js
@@ -1,7 +1,26 @@
 document.getElementById("search_button").addEventListener("click", getSelectedText);
-
+let textToSearch = '';
 function getSelectedText() {
   chrome.storage.sync.get('selectedText', function(data) {
-    document.testform.selectedtext.value = data.selectedText;
+    //for testing if the highlighted word is stored
+    // document.testform.selectedtext.value = data.selectedText;
+    textToSearch = data.selectedText;
+    if(textToSearch.length) {
+      SearchImage();
+    }
   })
+}
+
+async function SearchImage(){
+  let response = await fetch(`https://www.googleapis.com/customsearch/v1?q=${textToSearch}&key=AIzaSyAkCdD1ro_UiO05w8iqAdFjNCCCbgqnSlY&num=7&start=1&searchType=image&imgSize=medium&cx=014315630586445032606:me0hgspmvrs`);
+  let data = await response.json();
+  console.log(data.items[0].image.thumbnailLink,data.items[1].image.thumbnailLink, data.items[2].image.thumbnailLink);
+  setImage(data.items[0].image.thumbnailLink,data.items[1].image.thumbnailLink, data.items[2].image.thumbnailLink);
+  // console.log(data);
+}
+
+function setImage(source1, source2, source3){
+  document.getElementById("image1").src = source1;
+  document.getElementById("image2").src = source2;
+  document.getElementById("image3").src = source3;
 }
